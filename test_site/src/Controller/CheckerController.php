8a3f0f6844c8a1fa3767efc6954f008040b2c273
Peter Smith <pksmith137@gmail.com>
2023-08-10 10:52:13 +0100

    Update CheckerController route definitions to be more accurate

--- test_site/src/Controller/CheckerController.php
+++ test_site/src/Controller/CheckerController.php
@@ -4,25 +4,27 @@ namespace App\Controller;
 
 use App\Service\CheckerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/test')]
 class CheckerController extends AbstractController
 {
-    #[Route('/test/palindrome/{testWord}', name: 'test_palindrome', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
-    public function testIfPalindrome(CheckerService $checkerService, string $testWord = null): Response
+    #[Route('/palindrome/{testWord}', name: 'test_palindrome', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
+    public function testIfPalindrome(Request $request, CheckerService $checkerService, ?string $testWord = null): Response
     {
         return $this->render('index.html.twig');
     }
 
-    #[Route('/test/anagram/{testWord}', name: 'test_anagram', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
-    public function testIfAnagram(CheckerService $checkerService, string $testWord = null): Response
+    #[Route('/anagram/{testWord}/{comparison}', name: 'test_anagram', methods: ["GET", "POST"], requirements: ['testWord' => '\w+', 'comparison' => '\w+'])]
+    public function testIfAnagram(Request $request, CheckerService $checkerService, ?string $testWord = null, ?string $comparison = null): Response
     {
         return $this->render('index.html.twig');
     }
 
-    #[Route('/test/pangram/{testWord}', name: 'test_pangram', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
-    public function testIfPangram(CheckerService $checkerService, string $testWord = null): Response
+    #[Route('/pangram', name: 'test_pangram', methods: ["GET", "POST"])]
+    public function testIfPangram(Request $request, CheckerService $checkerService): Response
     {
         return $this->render('index.html.twig');
     }
