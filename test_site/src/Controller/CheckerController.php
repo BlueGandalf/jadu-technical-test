b2ac42ada452b3895edda14d778c5ef3b4d76142
Peter Smith <pksmith137@gmail.com>
2023-08-10 10:40:35 +0100

    Create a homepage view and create base routes for each of the problems

---
+++ test_site/src/Controller/CheckerController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CheckerService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckerController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/test/palindrome/{testWord}', name: 'test_palindrome', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
+    public function testIfPalindrome(CheckerService $checkerService, string $testWord = null): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/test/anagram/{testWord}', name: 'test_anagram', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
+    public function testIfAnagram(CheckerService $checkerService, string $testWord = null): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    #[Route('/test/pangram/{testWord}', name: 'test_pangram', methods: ["GET", "POST"], requirements: ['testWord' => '\w+'])]
+    public function testIfPangram(CheckerService $checkerService, string $testWord = null): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
